4a3c1c1e72a5026ae53e9ba073d5c0a33624f2c0
Sanskruti Mopari <sanskrutimopari2016@gmail.com>
2023-02-04 11:21:03 +0530

    Added: Functionality To Show Hello Message By Query Parameter

--- src/main/java/com/firstspringapp/controller/HelloController.java
+++ src/main/java/com/firstspringapp/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.firstspringapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello From Bridgelabz!!!";
+    }
+
+    @GetMapping("/query")
+    public String sayHelloParam(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
     }
 }
