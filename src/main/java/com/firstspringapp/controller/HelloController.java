8ac05b08c800eff34346eac4358b22e5085ef114
Sanskruti Mopari <sanskrutimopari2016@gmail.com>
2023-02-04 11:45:07 +0530

    Added: Functionality To Show Message By Pathvariable

--- src/main/java/com/firstspringapp/controller/HelloController.java
+++ src/main/java/com/firstspringapp/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.firstspringapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class HelloController {
 
     @GetMapping("/query")
     public String sayHelloParam(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHello(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 }
